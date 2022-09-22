e2e2670ef8f026fbf949f7d1e792b9c60918a23b
unknown <thanhlq@vissoft.vn>
2022-09-22 17:41:07 +0700

    first commit

---
+++ NhaTroToi_Project/src/main/java/vn/com/project/demo/NhaTroToiApplication.java
@@ -0,0 +1,13 @@
+package vn.com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhaTroToiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhaTroToiApplication.class, args);
+	}
+
+}
---
+++ NhaTroToi_Project/src/test/java/vn/com/project/demo/NhaTroToiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhaTroToiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
